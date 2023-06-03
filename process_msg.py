596e961a07fd04517e23181e4082d9699764ee66
JeronAlford44 <jalford3@lion.lmu.edu>
2023-06-03 16:35:48 -0700

    adding test queries

--- process_msg.py
+++ process_msg.py
@@ -2,4 +2,8 @@ from flask import jsonify
 def process_msg(message, name):
     if message == "Hello World":
         return jsonify({"msg" : f"Good day to you {name}"})
-    return jsonify({"msg" : "Sorry, I did not understand your message. Please enter a new message"})
+    if message == "How are you?":
+        return jsonify({"msg": "I am doing great, how about you?"})
+    if message is "What is my name?": 
+        return jsonify({"msg": f"The name you registered with is {name}"})
+    return jsonify({"msg" : "Thanks for logging in, how can I assist you today?"})
