434fb160a4cebd11d629417b5e759a0d7a5b05f1
JeronAlford44 <jalford3@lion.lmu.edu>
2023-06-05 20:05:00 -0700

    api now working

--- process_msg.py
+++ process_msg.py
@@ -4,6 +4,6 @@ def process_msg(message, name):
         return jsonify({"msg" : f"Good day to you {name}"})
     if message == "How are you?":
         return jsonify({"msg": "I am doing great, how about you?"})
-    if message is "What is my name?": 
+    if message == "What is my name?": 
         return jsonify({"msg": f"The name you registered with is {name}"})
     return jsonify({"msg" : "Thanks for logging in, how can I assist you today?"})
